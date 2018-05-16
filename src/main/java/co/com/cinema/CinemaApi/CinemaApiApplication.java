1f36cd2bc51f2a0dd6018ee007381affa0c665ce
parcero0818 <john.sgv@outlook.com>
2018-05-16 11:07:00 -0500

    primer commit

---
+++ src/main/java/co/com/cinema/CinemaApi/CinemaApiApplication.java
@@ -0,0 +1,12 @@
+package co.com.cinema.CinemaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaApiApplication.class, args);
+	}
+}
